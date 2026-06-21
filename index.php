707d909239dcdcde42cb73c4d3919e09b93e5e13
PritVK9411 <106378826@student.swin.edu.au>
2026-06-21 22:58:58 +0800

    Initial Commit

--- index.php
+++ index.php
@@ -35,33 +35,7 @@ require 'settings.php';
         <img src="Images/Volunteer.jpeg" alt="Volunteers working together">
         <figcaption>Our volunteers building impactful technology</figcaption>
     </figure>
-
-
-    <?php
-    try {
-        $stmt = $pdo->query("SELECT * FROM about ORDER BY id");
-        $members = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if (!empty($members)) {
-            echo '<section>';
-            echo '<h2>Team Contributions</h2>';
-            echo '<ul>';
-            foreach ($members as $member) {
-                echo '<li><strong>' . htmlspecialchars($member['member_name']) . '</strong><br>';
-                echo 'Project 1: ' . htmlspecialchars($member['project1_details']) . '<br>';
-                echo 'Project 2: ' . htmlspecialchars($member['project2_details']);
-                echo '</li>';
-            }
-            echo '</ul>';
-            echo '</section>';
-        } else {
-            echo '<section><p><em>No team contributions have been added yet.</em></p></section>';
-        }
-    } catch (PDOException $e) {
-        
-        echo '<section><p><em>Contributions table not set up yet. Please create the "about" table in phpMyAdmin.</em></p></section>';
-    }
-    ?>
+    
 </main>
 
 
